81a46b2631110aca4e6eb5f2b2dd815fb4e287a1
Wellington Cavalcante <wellarts@gmail.com>
2024-01-24 09:39:44 -0300

    teste3

--- app/Filament/Resources/ClienteResource.php
+++ app/Filament/Resources/ClienteResource.php
@@ -47,7 +47,7 @@ class ClienteResource extends Resource
                             ->required()
                             ->maxLength(255),
                         Forms\Components\TextInput::make('cpf_cnpj')
-                            ->label('CPF/CNPJ2')
+                            ->label('CPF/CNPJ')
                             ->mask(RawJs::make(<<<'JS'
                                     $input.length > 14 ? '99.999.999/9999-99' : '999.999.999-99'
                                 JS))
