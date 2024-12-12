c301ed3a25d2331183f11387e39d05546f1ae064
Wellington Cavalcante <wellarts@gmail.com>
2024-12-12 10:06:48 -0300

    pwq

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -7,7 +7,7 @@ return [
         'short_name' => 'Wsys Vendas',
         'start_url' => '/',
         'background_color' => '#050505',
-        'theme_color' => '#FFC300',
+        'theme_color' => '#FFE4B5',
         'display' => 'standalone',
         'orientation'=> 'any',
         'status_bar'=> 'black',
